963c8615ee2844bbb6430de2131f32e8e7b8f2a7
Itziar Alonso <ialonso@lst.tfo.upm.es>
2025-01-23 12:28:03 +0100

    before updating angular material

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,7 +2,9 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { BlankComponent } from './layouts/blank/blank.component';
 import { FullComponent } from './layouts/full/full.component';
-
+import { DataDiscoveryModule
+  
+ } from './pages/data-discovery/data-discovery.module';
 const routes: Routes = [
   {
     path: '',
@@ -27,18 +29,18 @@ const routes: Routes = [
     path: '',
     component: FullComponent,
     children: [
-      {
-        path: 'material-table',
-        loadChildren: () =>
-          import('./pages/pages.module').then((m) => m.PagesModule),
-      },
       // {
-      //   path: 'discovery',
+      //   path: 'material-table',
       //   loadChildren: () =>
-      //     import('./pages/data-discovery/data-discovery.module').then(
-      //       (m) => m.DataDiscoveryModule
-      //     ),
+      //     import('./pages/pages.module').then((m) => m.PagesModule),
       // },
+      {
+        path: 'discovery',
+        loadChildren: () =>
+          import('./pages/data-discovery/data-discovery.module').then(
+            (m) => m.DataDiscoveryModule
+          ),
+      },
     ],
   },
   
